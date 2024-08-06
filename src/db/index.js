945f23e73902539fc7a25adbbca3f2e469016d40
Kamal <kamalmehra293@gmail.com>
2024-08-06 17:31:48 +0530

    .then changes

--- src/db/index.js
+++ src/db/index.js
@@ -6,14 +6,15 @@ dotenv.config();
 const port = process.env.PORT;
 const connectionDB = async () => {
   try {
-    const connInstance = await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB connected successfully...",connInstance.connection.host);
+    const connInstance = await mongoose.connect(process.env.MONGODB_URI).then((con)=>{
+      console.log("MongoDB connected successfully...",con.connection.host);
       app.listen(port,()=>{
         console.log(`Server is running on port ${port}`)
       })
       app.get('/',(req,res)=>{
         res.send("hello world")
       })
+    })
   } catch (error) {
     console.log("The DataBase is Failed to Link",error)
   }
