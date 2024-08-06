084bdcf024647904a0af8e3457f2fca96416ec8c
Kamal <kamalmehra293@gmail.com>
2024-08-06 17:27:45 +0530

    new changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,22 @@
+import dotenv from "dotenv";
+import express from "express";
 import mongoose from "mongoose";
-
+const app = express();
+dotenv.config();
+const port = process.env.PORT;
 const connectionDB = async () => {
-  const connInstance = await mongoose.connect(process.env.MONGODB_URI);
-  
-  console.log(
-    "MongoDB connected successfully...",
-    connInstance.connection.host
-  );
+  try {
+    const connInstance = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected successfully...",connInstance.connection.host);
+      app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`)
+      })
+      app.get('/',(req,res)=>{
+        res.send("hello world")
+      })
+  } catch (error) {
+    console.log("The DataBase is Failed to Link",error)
+  }
 };
 
 export default connectionDB;
