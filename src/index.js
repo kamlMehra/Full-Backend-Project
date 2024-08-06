2aa992f8e24c7e1600b77af67e7b36c8306fbdf9
Kamal <kamalmehra293@gmail.com>
2024-08-06 17:27:21 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import dotenv from "dotenv";
-import express from "express";
-import connectionDB from "./db/index.js";
-const app = express();
-dotenv.config();
-const port = process.env.PORT;
-connectionDB().then(() => {
-  app.listen(port, () => {
-    console.log(`The port is listening on ${port}`);
-  });
-});
+import connectionDB from "./db/index.js"
+
+connectionDB();
